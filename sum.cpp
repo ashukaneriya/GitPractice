ec91551a970300904e5f6ed3a46405778aef4593
Ashutosh Kaneriya <ashukaneriya@gmail.com>
2024-10-10 11:28:09 +0530

    Initial commit

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter number1: ";
+    cin >> a;
+    cout << "Enter number2: ";
+    cin >> b;
+    int sum = a+b;
+    cout << "Sum of number1 + number2 = " << sum << endl;
+    cin >> a;
+
+    return sum;
+}
